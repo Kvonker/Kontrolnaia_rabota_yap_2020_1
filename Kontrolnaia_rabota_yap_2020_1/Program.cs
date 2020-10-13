82b1775cc5743d834a333fe3600dce264b81345c
blackkvonker@mail.ru <blackkvonker@mail.ru>
2020-10-13 18:15:56 +0300

    Добавьте файлы проекта.

---
+++ Kontrolnaia_rabota_yap_2020_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kontrolnaia_rabota_yap_2020_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
