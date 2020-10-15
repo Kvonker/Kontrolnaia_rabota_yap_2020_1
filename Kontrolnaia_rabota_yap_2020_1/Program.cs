04f5f7b32eb86252033e1132c56894ab3eeaa824
blackkvonker@mail.ru <blackkvonker@mail.ru>
2020-10-15 19:02:08 +0300

    1

--- Kontrolnaia_rabota_yap_2020_1/Program.cs
+++ Kontrolnaia_rabota_yap_2020_1/Program.cs
@@ -1,82 +1,244 @@
 ﻿using System;
 using System.Data;
 using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Kontrolnaia_rabota_yap_2020_1
 {
     class Program
     {
-        class Person
-        {
-            private string name;
-            private string secondname;
-            private System.DateTime BDate;
-
-            public Person(string personname, string personsecondname, DateTime personBDate)
-            {
-                this.name = personname;
-                this.secondname = personsecondname;
-                this.BDate = personBDate;
-            }
-
-            private string perpsonname;
-            public string Name
-            {
-                get
-                {
-                    return name;
-                }
-            }
-
-            private string perpsonsecondname;
-            public string SecondName
-            {
-                get
-                {
-                    return name;
-                }
-            }
-
-            DateTime personBDate
-            {
-                get
-                {
-                    return BDate;
-                }
-            }
-
-            int intpersonBDate
-            {
-                get
-                {
-                    return Convert.ToInt32(BDate);
-                }
-
-                set
-                {
-                    BDate = Convert.ToDateTime(value);
-                }
-            }
-
-            public override string ToString()
-            {
-                return string.Format("{0} {1}\nDate of birth: {2}", Name, SecondName, BDate);
-            }
-
-            public string ToShortString()
-            {
-                return "\n" + "Имя: " + Name + "\n" + "Фамилия: " + SecondName;
-            }
-
-        }
         static void Main(string[] args)
         {
-            int nrow = 2;
-            int ncolumn = 3;
-            int[] mass_1 = new int[nrow * ncolumn];
-            int[,] mass_2 = new int[nrow, ncolumn];
-
-            Console.WriteLine("Hello World!");
+            ResearchTeam std = new ResearchTeam(); 
+            Console.WriteLine(std.ToShortString()); 
         }
     }
+    class Person
+    {
+        private 
+
+        string name;
+        string secondname;
+        System.DateTime BDate;
+
+        public Person(string personname, string personsecondname, DateTime personBDate)
+        {
+            name = personname;
+            secondname = personsecondname;
+            BDate = personBDate;
+        }
+
+        public Person() : this("Default_Name", "Default_Sname", new DateTime(1990, 22, 13))
+        { }
+
+        public string PersonName
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string PersonSecondName
+        {
+            get
+            {
+                return secondname;
+            }
+        }
+
+        DateTime PersonBDate
+        {
+            get
+            {
+                return BDate;
+            }
+        }
+
+        int intPersonBDate
+        {
+            get
+            {
+                return Convert.ToInt32(BDate);
+            }
+
+            set
+            {
+                BDate = Convert.ToDateTime(value);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}\nDate of birth: {2}", name, secondname, BDate);
+        }
+
+        public string ToShortString()
+        {
+            return "\n" + "Имя: " + name + "\n" + "Фамилия: " + secondname;
+        }
+
+    }
+
+    enum TimeFrame {Year,TwoYears,Long}
+
+    class Paper
+    {
+        public
+
+        string NameP { get; set; }
+        Person Author { get; set; } 
+        DateTime Data { get; set; } 
+
+        public Paper(string name, Person author, DateTime data)
+        {
+            NameP = name;
+            Author = author;
+            Data = data;
+        }
+
+        // конструктор без параметров, инициализирующий все свойства класса некоторыми значениями по умолчанию
+        private Paper() : this("WarandWorld", "Lolstoy", new DateTime(1999, 10, 1))
+        { }
+
+
+           
+        public override string ToString()
+        {
+            return string.Format("Author  {0} write book {1}. Data = {2}", NameP, Author, Data);
+        }
+    }
+
+    class ResearchTeam
+    {
+        private string Theme; 
+        private string NameOfOrg; 
+        private int NumberOfRed; 
+        private TimeFrame Last;
+        private List<Paper> Papers = new List<Paper>();
+
+
+        //В классе ResearchTeam определить конструкторы
+        //Конструктор c параметрами типа string, string, int, TimeFrame для инициализации соответствующих полей класса
+
+
+        public ResearchTeam(string theme, string nameoforg, int numberofred, TimeFrame last)
+        {
+            Theme = theme;
+            NameOfOrg = nameoforg;
+            NumberOfRed = numberofred;
+            Last = last;
+        }
+
+        //Конструктор без параметров, инициализирующий поля класса значениями по умолчанию
+        public ResearchTeam(): this(new Person("Ecology", "Colos", 0123456789, new TimeFrame(15), Paper.Spe * *****t, 1) //конструктор без параметров, инициализирующий поля класса значениями по умолчанию
+        { }
+    
+
+    //В классе ResearchTeam определить свойства c методами get и set
+    //Cвойство типа string для доступа к полю с названием темы исследова-ний
+        public string theme
+        {
+            get
+            {
+                return Theme;
+            }
+        }
+
+        //Cвойство типа string для доступа к полю с названием организации
+        public string nameofogr
+        {
+            get
+            {
+                return NameOfOrg;
+            }
+        }
+
+        //Cвойство типа int для доступа к полю с номером регистрации
+        public int numberofred
+        {
+            get
+            {
+                return NumberOfRed;
+            }
+        }
+
+        //Cвойство типа TimeFrame для доступа к полю с продолжительностью ис-следований
+        public TimeFrame Last
+        {
+            get
+            {
+                return last;
+            }
+        }
+
+        //Cвойство типа Paper[] для доступа к полю со списком публикаций по те-ме исследований
+        public IReadOnlyList<Paper> Publications // свойство типа Exam [] для доступа к полю со списком экзаменов
+        {
+            get
+            {
+                return _publications.AsReadOnly();
+            }
+        }
+
+
+        //Cвойство типа Paper[] для доступа к полю со списком публикаций по те-ме исследований
+        //Cвойство типа Paper ( только с методом get), которое возвращает ссылку на публикацию с самой поздней датой выхода; если список публикаций пустой, свойство возвращает значение null
+        public double ListPublic
+        {
+            get
+            {
+                /*double averageGrade = _passedExams.Average(avgrade => avgrade.Grade);
+                return averageGrade;*/
+                return null;
+            }
+        }
+
+
+        //Индексатор булевского типа (только с методом get) с одним параметром типа Education; значение индексатора равно true, если значение поля с формой обучения студента совпадает со значением индекса, и false в противном случае
+        public bool this[TimeFrame rez_prov]
+        {
+            get
+            {
+                bool rez;
+                if (rez_prov == TimeFrame) rez = true;
+                else rez = false;
+                return rez;
+            }
+        }
+
+
+        //Метод void AddPapers ( params Paper[] ) для добавления элементов в список публикаций
+        public void AddPapers(params Paper[] )
+        {
+            _publications.AddRange();
+        }
+
+
+        //Gерегруженную версию виртуального метода string ToString() для фор-мирования строки со значениями всех полей класса, включая список пуб-ликаций
+        public override string ToString()
+        {
+            return string.Format("\nTheme: {0}\nNameOfOrg: {1}\nNumberOfRed: {2}\nLast: {3}\nPublications: {4} ", Theme, NameOfOrg, NumberOfRed, Last, _publications);
+        }
+
+        //Виртуальный метод string ToShortString(), который формирует строку со значениями всех полей класса без списка публикаций
+        public string ToShortString() // виртуальный метод string ToShortString(), который формирует строку со значениями всех полей класса без списка экзаменов, но со значением среднего балла.
+        {
+            return string.Format("\nTheme: {0}\nNameOfOrg: {1}\nNumberOfRed: {2}\nLast: {3}\n", Theme, NameOfOrg, NumberOfRed, Last);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int nrow = 2;
+        int ncolumn = 3;
+        int[] mass_1 = new int[nrow * ncolumn];
+        int[,] mass_2 = new int[nrow, ncolumn];
+
+        Console.WriteLine("Hello World!");
+    }
+
 }
